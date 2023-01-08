55c1e705788bb4c72639a743abcc5727d76e53e9
K.Rohit <rohitk@zen3.co.uk>
2023-01-08 18:06:28 +0530

    Add project files.

---
+++ EmployeeForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace PayrollApplication
+{
+    partial class EmployeeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpEmployeeInformation = new System.Windows.Forms.GroupBox();
+            this.grpEmployeeContactDetails = new System.Windows.Forms.GroupBox();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.grpGender = new System.Windows.Forms.GroupBox();
+            this.rdbMale = new System.Windows.Forms.RadioButton();
+            this.rdbFemale = new System.Windows.Forms.RadioButton();
+            this.txtNationalInsuranceNo = new System.Windows.Forms.TextBox();
+            this.lblNationalInsuranceNo = new System.Windows.Forms.Label();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.rdbSingle = new System.Windows.Forms.RadioButton();
+            this.rdbMarried = new System.Windows.Forms.RadioButton();
+            this.grpMartialStatus = new System.Windows.Forms.GroupBox();
+            this.lblUnionMember = new System.Windows.Forms.Label();
+            this.chkUnionMember = new System.Windows.Forms.CheckBox();
+            this.grpEmployeeInformation.SuspendLayout();
+            this.grpGender.SuspendLayout();
+            this.grpMartialStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpEmployeeInformation
+            // 
+            this.grpEmployeeInformation.Controls.Add(this.chkUnionMember);
+            this.grpEmployeeInformation.Controls.Add(this.lblUnionMember);
+            this.grpEmployeeInformation.Controls.Add(this.grpMartialStatus);
+            this.grpEmployeeInformation.Controls.Add(this.dtpDateOfBirth);
+            this.grpEmployeeInformation.Controls.Add(this.lblDateOfBirth);
+            this.grpEmployeeInformation.Controls.Add(this.txtNationalInsuranceNo);
+            this.grpEmployeeInformation.Controls.Add(this.lblNationalInsuranceNo);
+            this.grpEmployeeInformation.Controls.Add(this.grpGender);
+            this.grpEmployeeInformation.Controls.Add(this.txtFirstName);
+            this.grpEmployeeInformation.Controls.Add(this.lblFirstName);
+            this.grpEmployeeInformation.Controls.Add(this.txtLastName);
+            this.grpEmployeeInformation.Controls.Add(this.lblLastName);
+            this.grpEmployeeInformation.Controls.Add(this.txtEmployeeID);
+            this.grpEmployeeInformation.Controls.Add(this.lblEmployeeID);
+            this.grpEmployeeInformation.Location = new System.Drawing.Point(30, 31);
+            this.grpEmployeeInformation.Name = "grpEmployeeInformation";
+            this.grpEmployeeInformation.Size = new System.Drawing.Size(447, 571);
+            this.grpEmployeeInformation.TabIndex = 0;
+            this.grpEmployeeInformation.TabStop = false;
+            this.grpEmployeeInformation.Text = "Employee Information";
+            // 
+            // grpEmployeeContactDetails
+            // 
+            this.grpEmployeeContactDetails.Location = new System.Drawing.Point(527, 31);
+            this.grpEmployeeContactDetails.Name = "grpEmployeeContactDetails";
+            this.grpEmployeeContactDetails.Size = new System.Drawing.Size(447, 571);
+            this.grpEmployeeContactDetails.TabIndex = 1;
+            this.grpEmployeeContactDetails.TabStop = false;
+            this.grpEmployeeContactDetails.Text = "Employee Contact Details";
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.AutoSize = true;
+            this.lblEmployeeID.Location = new System.Drawing.Point(24, 47);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(108, 20);
+            this.lblEmployeeID.TabIndex = 0;
+            this.lblEmployeeID.Text = "Employee ID :";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(212, 44);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(187, 26);
+            this.txtEmployeeID.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(212, 136);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(187, 26);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(24, 139);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(90, 20);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "LastName :";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(212, 87);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(187, 26);
+            this.txtFirstName.TabIndex = 5;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(24, 90);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(94, 20);
+            this.lblFirstName.TabIndex = 4;
+            this.lblFirstName.Text = "First Name :";
+            // 
+            // grpGender
+            // 
+            this.grpGender.Controls.Add(this.rdbFemale);
+            this.grpGender.Controls.Add(this.rdbMale);
+            this.grpGender.Location = new System.Drawing.Point(28, 181);
+            this.grpGender.Name = "grpGender";
+            this.grpGender.Size = new System.Drawing.Size(371, 100);
+            this.grpGender.TabIndex = 6;
+            this.grpGender.TabStop = false;
+            this.grpGender.Text = "Gender :";
+            // 
+            // rdbMale
+            // 
+            this.rdbMale.AutoSize = true;
+            this.rdbMale.Location = new System.Drawing.Point(102, 38);
+            this.rdbMale.Name = "rdbMale";
+            this.rdbMale.Size = new System.Drawing.Size(68, 24);
+            this.rdbMale.TabIndex = 0;
+            this.rdbMale.TabStop = true;
+            this.rdbMale.Text = "Male";
+            this.rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbFemale
+            // 
+            this.rdbFemale.AutoSize = true;
+            this.rdbFemale.Location = new System.Drawing.Point(247, 38);
+            this.rdbFemale.Name = "rdbFemale";
+            this.rdbFemale.Size = new System.Drawing.Size(87, 24);
+            this.rdbFemale.TabIndex = 1;
+            this.rdbFemale.TabStop = true;
+            this.rdbFemale.Text = "Female";
+            this.rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtNationalInsuranceNo
+            // 
+            this.txtNationalInsuranceNo.Location = new System.Drawing.Point(212, 304);
+            this.txtNationalInsuranceNo.Name = "txtNationalInsuranceNo";
+            this.txtNationalInsuranceNo.Size = new System.Drawing.Size(187, 26);
+            this.txtNationalInsuranceNo.TabIndex = 8;
+            // 
+            // lblNationalInsuranceNo
+            // 
+            this.lblNationalInsuranceNo.AutoSize = true;
+            this.lblNationalInsuranceNo.Location = new System.Drawing.Point(24, 307);
+            this.lblNationalInsuranceNo.Name = "lblNationalInsuranceNo";
+            this.lblNationalInsuranceNo.Size = new System.Drawing.Size(174, 20);
+            this.lblNationalInsuranceNo.TabIndex = 7;
+            this.lblNationalInsuranceNo.Text = "National Insurance No :";
+            // 
+            // lblDateOfBirth
+            // 
+            this.lblDateOfBirth.AutoSize = true;
+            this.lblDateOfBirth.Location = new System.Drawing.Point(24, 354);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(110, 20);
+            this.lblDateOfBirth.TabIndex = 9;
+            this.lblDateOfBirth.Text = "Date Of Birth :";
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(212, 354);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(187, 26);
+            this.dtpDateOfBirth.TabIndex = 10;
+            // 
+            // rdbSingle
+            // 
+            this.rdbSingle.AutoSize = true;
+            this.rdbSingle.Location = new System.Drawing.Point(247, 38);
+            this.rdbSingle.Name = "rdbSingle";
+            this.rdbSingle.Size = new System.Drawing.Size(78, 24);
+            this.rdbSingle.TabIndex = 1;
+            this.rdbSingle.TabStop = true;
+            this.rdbSingle.Text = "Single";
+            this.rdbSingle.UseVisualStyleBackColor = true;
+            // 
+            // rdbMarried
+            // 
+            this.rdbMarried.AutoSize = true;
+            this.rdbMarried.Location = new System.Drawing.Point(102, 38);
+            this.rdbMarried.Name = "rdbMarried";
+            this.rdbMarried.Size = new System.Drawing.Size(87, 24);
+            this.rdbMarried.TabIndex = 0;
+            this.rdbMarried.TabStop = true;
+            this.rdbMarried.Text = "Married";
+            this.rdbMarried.UseVisualStyleBackColor = true;
+            // 
+            // grpMartialStatus
+            // 
+            this.grpMartialStatus.Controls.Add(this.rdbSingle);
+            this.grpMartialStatus.Controls.Add(this.rdbMarried);
+            this.grpMartialStatus.Location = new System.Drawing.Point(28, 400);
+            this.grpMartialStatus.Name = "grpMartialStatus";
+            this.grpMartialStatus.Size = new System.Drawing.Size(371, 100);
+            this.grpMartialStatus.TabIndex = 7;
+            this.grpMartialStatus.TabStop = false;
+            this.grpMartialStatus.Text = "Martial Status :";
+            // 
+            // lblUnionMember
+            // 
+            this.lblUnionMember.AutoSize = true;
+            this.lblUnionMember.Location = new System.Drawing.Point(24, 526);
+            this.lblUnionMember.Name = "lblUnionMember";
+            this.lblUnionMember.Size = new System.Drawing.Size(121, 20);
+            this.lblUnionMember.TabIndex = 11;
+            this.lblUnionMember.Text = "Union Member :";
+            // 
+            // chkUnionMember
+            // 
+            this.chkUnionMember.AutoSize = true;
+            this.chkUnionMember.Location = new System.Drawing.Point(212, 522);
+            this.chkUnionMember.Name = "chkUnionMember";
+            this.chkUnionMember.Size = new System.Drawing.Size(22, 21);
+            this.chkUnionMember.TabIndex = 12;
+            this.chkUnionMember.UseVisualStyleBackColor = true;
+            // 
+            // EmployeeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(998, 710);
+            this.Controls.Add(this.grpEmployeeContactDetails);
+            this.Controls.Add(this.grpEmployeeInformation);
+            this.Name = "EmployeeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manage Employee";
+            this.grpEmployeeInformation.ResumeLayout(false);
+            this.grpEmployeeInformation.PerformLayout();
+            this.grpGender.ResumeLayout(false);
+            this.grpGender.PerformLayout();
+            this.grpMartialStatus.ResumeLayout(false);
+            this.grpMartialStatus.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpEmployeeInformation;
+        private System.Windows.Forms.CheckBox chkUnionMember;
+        private System.Windows.Forms.Label lblUnionMember;
+        private System.Windows.Forms.GroupBox grpMartialStatus;
+        private System.Windows.Forms.RadioButton rdbSingle;
+        private System.Windows.Forms.RadioButton rdbMarried;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.TextBox txtNationalInsuranceNo;
+        private System.Windows.Forms.Label lblNationalInsuranceNo;
+        private System.Windows.Forms.GroupBox grpGender;
+        private System.Windows.Forms.RadioButton rdbFemale;
+        private System.Windows.Forms.RadioButton rdbMale;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.GroupBox grpEmployeeContactDetails;
+    }
+}
+
